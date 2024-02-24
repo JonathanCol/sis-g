b74a4cf3be00881fe182798f10399f674a1fd2a8
JonathanCol <jonathancediel@outlook.com>
2024-02-24 12:29:33 -0500

    FIX: se restableció la seccion home

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import StartContentHome from "../app/components/Home/StartContentHome";
+import WelcomeSection from "../app/components/WelcomeSection/WelcomeSection";
+import RootLayout from "../app/layout";
+
+
+
+export default function Home() {
+  return (
+     <>
+     <RootLayout>
+      <WelcomeSection>
+        <StartContentHome />
+      </WelcomeSection>
+     </RootLayout>
+     </>
+  );
+}
